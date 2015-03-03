f5565d8d994957f43729116ad1f46564da460468
vitoo23 <xaavi.91@gmail.com>
2015-03-03 16:22:26 +0100

    1er commit

---
+++ Totmotor/Totmotor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Totmotor.Startup))]
+namespace Totmotor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
